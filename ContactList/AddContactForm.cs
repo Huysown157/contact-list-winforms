c9b370a403e0cb3e6b61933d51ce7110f5b73999
vannho20 <nhodepzai7@gmail.com>
2025-05-27 09:44:59 +0700

    Thêm Form để thêm liên hệ mới

--- ContactList/AddContactForm.cs
+++ ContactList/AddContactForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ContactList
 {
-    public partial class Form1 : Form
+    public partial class AddContactForm : Form
     {
-        public Form1()
+        public AddContactForm()
         {
             InitializeComponent();
         }
--- ContactList/MainForm.Designer.cs
+++ ContactList/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ContactList
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,18 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnThemLienHe = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btnThemLienHe
             // 
-            this.button1.Location = new System.Drawing.Point(148, 73);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Them Lien He";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnThemLienHe.Location = new System.Drawing.Point(148, 73);
+            this.btnThemLienHe.Name = "btnThemLienHe";
+            this.btnThemLienHe.Size = new System.Drawing.Size(156, 23);
+            this.btnThemLienHe.TabIndex = 0;
+            this.btnThemLienHe.Text = "Them Lien He";
+            this.btnThemLienHe.UseVisualStyleBackColor = true;
+            this.btnThemLienHe.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -50,15 +51,16 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Danh Sach Lien He";
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Controls.Add(this.btnThemLienHe);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,7 +68,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnThemLienHe;
         private System.Windows.Forms.Label label1;
     }
 }
